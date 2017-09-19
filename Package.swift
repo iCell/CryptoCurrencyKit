a479d2e626cb6f97f160549e279f2a8a9efa4199
XiaoyuLi <xiaoyu.li@shanbay.com>
2017-09-19 16:26:51 +0800

    update Package.swift to version 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,18 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CryptoCurrencyKit",
-    exclude: ["Sources/CryptoCurrencyKit/CryptoCurrencyKit.swift",
-              "Sources/CryptoCurrencyKit/Ticker.swift",
-              "Sources/CryptoCurrencyKit/Global.swift"]
+    products: [
+        .library(
+            name: "CryptoCurrencyKit",
+            targets: ["CryptoCurrencyKit"]),
+        ],
+    targets: [
+        .target(
+            name: "CryptoCurrencyKit",
+            path: ".",
+            sources: ["Sources"])
+    ]
 )
